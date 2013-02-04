dfd65911ea05225285f3c8913b9400d2c336e400
nathaniel k smith <nathanielksmith@gmail.com>
2013-02-04 15:33:00 -0500

    add scoping

--- paint_black.js
+++ paint_black.js
@@ -1,11 +1,12 @@
-var black = '#000000';
-var $body = cmg.query('body');
+(function() {
+    var black = '#000000';
+    var $body = cmg.query('body');
 
-var paint_black = function() {
-    $body.css('background-color', black);
-};
+    var paint_black = function() {
+        $body.css('background-color', black);
+    };
 
-// side-effect when file is included
-cmg.query(function() {
-    $body.on('click', paint_black);
-});
+    cmg.query(function() {
+        $body.on('click', paint_black);
+    });
+})();
